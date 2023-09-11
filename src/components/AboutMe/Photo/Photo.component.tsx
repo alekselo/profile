3968010be8d6bed37a095d204bc43a7c1286193c
Aleksey Loktev <a.a.loktev@yandex.ru>
2023-09-11 13:54:03 +0300

    refact: AboutMe component

---
+++ src/components/AboutMe/Photo/Photo.component.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export const Photo = () => {
+  return (
+    <div className={styles.aboutPhotoContainer}>
+      <div className={styles.aboutPhoto} />
+    </div>
+  );
+};
